6074284ea8a8a14c94b4d9d697ecfd76867421a2
Arbee4ever <arbee@arbeeco.de>
2024-03-01 23:32:56 +0100

    - fix metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { page } from '$app/stores';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
+	import ArbeeLogo from "$lib/img/2022-01_ARBEE_Bild-Marke_rgb_Verlauf_01.png"
 
 	if (browser) {
 		if ($page.url.searchParams.get('do') == 'abarrelroll') {
@@ -15,20 +16,20 @@
 
 <svelte:head>
 	<meta charset="utf-8" />
-	<title>Statcord</title>
-	<meta name="description" content="Just another leveling Bot with some unique Features." />
+	<title>Modrinth Collections Helper | ARBEE</title>
+	<meta name="description" content="A Tool to help you with Modrinth Collections." />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<!--<link rel="icon" href={StatcordLogoRound} />-->
-	<link rel="canonical" href="https://statcord.arbeeco.de" />
+	<link rel="icon" href={ArbeeLogo} />
+	<link rel="canonical" href="https://modcollhelper.arbeeco.de" />
 	<meta name="twitter:card" content="summary" />
-	<meta name="twitter:site" content="@0Arbee0" />
-	<meta name="twitter:creator" content="@0Arbee0" />
+	<meta name="twitter:site" content="@ARBEE_codes" />
+	<meta name="twitter:creator" content="@ARBEE_codes" />
 	<meta property="og:url" content="https://arbeeco.de" />
-	<meta property="og:title" content="Statcord" />
-	<meta property="og:description" content="Just another leveling Bot with some unique Features." />
-	<!--<meta property="og:image" content={StatcordLogo} />-->
+	<meta property="og:title" content="Modrinth Collections Helper" />
+	<meta property="og:description" content="A Tool to help you with Modrinth Collections." />
+	<meta property="og:image" content={ArbeeLogo} />
 	<meta name="theme-color" content="#2b8eaf" />
-	<meta name="keywords" content="Programming, Bot, Statcord, Discord" />
+	<meta name="keywords" content="Programming, Modrinth, Statcord, Minecraft, Mods" />
 	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1252158636066695"
 					crossorigin="anonymous"></script>
 </svelte:head>
